88f85037217c94ca03dc50499ed0daafb257b32b
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-07-09 16:32:02 -0400

    changed imgs to divs to fix image appearance and changed scoring

--- main.js
+++ main.js
@@ -1,64 +1,83 @@
 $(document).ready(function() {
 	// alert("Are you feeling lucky today?");
-	// 6 seconds, 1600px
+	// 6 seconds, 1900px
 
 	$("#score-display").text(0);
 
 	$( "#duck1a" ).click(function() {
-	  	$("#score-display").html(function(i, val) { return +val+1 });
-      $("#duck1a").css("display", "none");
+	  	$("#score-display").html(function(i, val) { return +val+5 });
+	  $("#duck1a").css("display", "none");
 	});
 
-  $( "#duck1b" ).click(function() {
-      $("#score-display").html(function(i, val) { return +val+1 });
-      $("#duck1b").css("display", "none");
-  });
+	$( "#duck1b" ).click(function() {
+	  $("#score-display").html(function(i, val) { return +val+10 });
+	  $("#duck1b").css("display", "none");
+	});
 
-   $( "#duck1c" ).click(function() {
-      $("#score-display").html(function(i, val) { return +val+1 });
-      $("#duck1c").css("display", "none");
-  });
+	$( "#duck1c" ).click(function() {
+	  $("#score-display").html(function(i, val) { return +val+10 });
+	  $("#duck1c").css("display", "none");
+	});
 
-  $( "#duck2b" ).click(function() {
-      $("#score-display").html(function(i, val) { return +val+1 });
-      $("#duck2b").css("display", "none");
-  });
+	$( "#duck2a" ).click(function() {
+	  	$("#score-display").html(function(i, val) { return +val-5 });
+	  $("#duck2a").css("display", "none");
+	});
 
-  $( "#duck3a" ).click(function() {
-      $("#score-display").html(function(i, val) { return +val+1 });
-      $("#duck3a").css("display", "none");
-  });
+	$( "#duck2b" ).click(function() {
+	  $("#score-display").html(function(i, val) { return +val+5 });
+	  $("#duck2b").css("display", "none");
+	});
 
-  $( "#duck3c" ).click(function() {
-      $("#score-display").html(function(i, val) { return +val+1 });
-      $("#duck3c").css("display", "none");
-  });
+	$( "#duck2c" ).click(function() {
+	  $("#score-display").html(function(i, val) { return +val-10 });
+	  $("#duck2c").css("display", "none");
+	});
 
-  $( "#duck4a" ).click(function() {
-      $("#score-display").html(function(i, val) { return +val+1 });
-      $("#duck4a").css("display", "none");
-  });
+	$( "#duck3a" ).click(function() {
+	  $("#score-display").html(function(i, val) { return +val+5 });
+	  $("#duck3a").css("display", "none");
+	});
 
-  $( "#duck4b" ).click(function() {
-      $("#score-display").html(function(i, val) { return +val+1 });
-      $("#duck4b").css("display", "none");
-  });
+	$( "#duck3b" ).click(function() {
+	  $("#score-display").html(function(i, val) { return +val-5 });
+	  $("#duck3b").css("display", "none");
+	});
 
+	$( "#duck3c" ).click(function() {
+	  $("#score-display").html(function(i, val) { return +val+10 });
+	  $("#duck3c").css("display", "none");
+	});
+
+	$( "#duck4a" ).click(function() {
+	  $("#score-display").html(function(i, val) { return +val+10 });
+	  $("#duck4a").css("display", "none");
+	});
+
+	$( "#duck4b" ).click(function() {
+	  $("#score-display").html(function(i, val) { return +val+5 });
+	  $("#duck4b").css("display", "none");
+	});
+
+	$( "#duck4c" ).click(function() {
+	  $("#score-display").html(function(i, val) { return +val-10 });
+	  $("#duck4c").css("display", "none");
+	});
 
 
 	$( "#clickme" ).click(function() {
   		$( "#duck1a" ).animate({
    		 	left: "+=500",
+  		}, 2000, function() {
+  		});
+
+  		$( "#duck1a" ).animate({
+   		 	left: "+=700",
   		}, 1000, function() {
   		});
 
   		$( "#duck1a" ).animate({
-   		 	left: "+=500",
-  		}, 1000, function() {
-  		});
-
-  		$( "#duck1a" ).animate({
-   		 	left: "+=100",
+   		 	left: "+=200",
   		}, 2000, function() {
   		});
 
@@ -69,16 +88,16 @@ $(document).ready(function() {
 
 	// GO BACK HOME DUCKS
   		$( "#duck1a" ).animate({
-   			left: "-=1600",
+   			left: "-=1900",
   			}, 1000, function() {
-          $("#duck1a").css("display", "initial");
+          $("#duck1a").css("display", "inline-block");
  		});
 	});
 
 
 	$( "#clickme" ).click(function() {
   		$( "#duck1b" ).animate({
-   		 	left: "+=20",
+   		 	left: "+=120",
   		}, 3000, function() {
   		});
 
@@ -88,50 +107,50 @@ $(document).ready(function() {
   		});
 
   		$( "#duck1b" ).animate({
-   		 	left: "+=100",
-  		}, 1000, function() {
-  		});
-
-  		$( "#duck1b" ).animate({
-   		 	left: "+=680",
-  		}, 1000, function() {
-  		});
-
-	// GO BACK HOME DUCKS
-  		$( "#duck1b" ).animate({
-   			left: "-=1600",
-  			}, 1000, function() {
-          $("#duck1b").css("display", "initial");
- 		});
-	});
-
-
-	$( "#clickme" ).click(function() {
-  		$( "#duck1c" ).animate({
-   		 	left: "+=300",
-  		}, 3000, function() {
-  		});
-
-  		$( "#duck1c" ).animate({
-   		 	left: "+=300",
-  		}, 1000, function() {
-  		});
-
-  		$( "#duck1c" ).animate({
-   		 	left: "+=800",
-  		}, 1000, function() {
-  		});
-
-  		$( "#duck1c" ).animate({
    		 	left: "+=200",
   		}, 1000, function() {
   		});
 
+  		$( "#duck1b" ).animate({
+   		 	left: "+=780",
+  		}, 1000, function() {
+  		});
+
+	// GO BACK HOME DUCKS
+  		$( "#duck1b" ).animate({
+   			left: "-=1900",
+  			}, 1000, function() {
+          $("#duck1b").css("display", "inline-block");
+ 		});
+	});
+
+
+	$( "#clickme" ).click(function() {
+  		$( "#duck1c" ).animate({
+   		 	left: "+=200",
+  		}, 3000, function() {
+  		});
+
+  		$( "#duck1c" ).animate({
+   		 	left: "+=500",
+  		}, 1000, function() {
+  		});
+
+  		$( "#duck1c" ).animate({
+   		 	left: "+=900",
+  		}, 1000, function() {
+  		});
+
+  		$( "#duck1c" ).animate({
+   		 	left: "+=300",
+  		}, 1000, function() {
+  		});
+
 	// GO BACK HOME DUCKS
   		$( "#duck1c" ).animate({
-   			left: "-=1600",
+   			left: "-=1900",
   			}, 1000, function() {
-          $("#duck1c").css("display", "initial");
+          $("#duck1c").css("display", "inline-block");
  		});
 	});
 
@@ -143,24 +162,25 @@ $(document).ready(function() {
   		});
 
   		$( "#duck2a" ).animate({
-   		 	left: "+=800",
+   		 	left: "+=900",
   		}, 2000, function() {
   		});
 
   		$( "#duck2a" ).animate({
-   		 	left: "+=200",
+   		 	left: "+=300",
   		}, 1000, function() {
   		});
 
   		$( "#duck2a" ).animate({
-   		 	left: "+=550",
+   		 	left: "+=650",
   		}, 2000, function() {
   		});
 
 	// GO BACK HOME DUCKS
   		$( "#duck2a" ).animate({
-   			left: "-=1600",
+   			left: "-=1900",
   			}, 1000, function() {
+  		  $("#duck2a").css("display", "inline-block");
  		});
 	});
 
@@ -172,25 +192,25 @@ $(document).ready(function() {
   		});
 
   		$( "#duck2b" ).animate({
+   		 	left: "+=800",
+  		}, 2000, function() {
+  		});
+
+  		$( "#duck2b" ).animate({
+   		 	left: "+=100",
+  		}, 2000, function() {
+  		});
+
+  		$( "#duck2b" ).animate({
    		 	left: "+=600",
-  		}, 2000, function() {
-  		});
-
-  		$( "#duck2b" ).animate({
-   		 	left: "+=200",
-  		}, 2000, function() {
-  		});
-
-  		$( "#duck2b" ).animate({
-   		 	left: "+=400",
   		}, 1000, function() {
   		});
 
   	// GO BACK HOME DUCKS
   		$( "#duck2b" ).animate({
-   			left: "-=1600",
+   			left: "-=1900",
   			}, 1000, function() {
-          $("#duck2b").css("display", "initial");
+          $("#duck2b").css("display", "inline-block");
  		});
 	});
 
@@ -206,19 +226,20 @@ $(document).ready(function() {
   		});
 
   		$( "#duck2c" ).animate({
-   		 	left: "+=100",
+   		 	left: "+=200",
   		}, 2000, function() {
   		});
 
   		$( "#duck2c" ).animate({
-   		 	left: "+=700",
+   		 	left: "+=900",
   		}, 1000, function() {
   		});
 
 	// GO BACK HOME DUCKS
   		$( "#duck2c" ).animate({
-   			left: "-=1600",
+   			left: "-=1900",
   			}, 1000, function() {
+  		  $("#duck2c").css("display", "inline-block");
  		});
 	});
 
@@ -235,20 +256,20 @@ $(document).ready(function() {
   		});
 
   		$( "#duck3a" ).animate({
-   		 	left: "+=100",
+   		 	left: "+=200",
   		}, 500, function() {
   		});
 
   		$( "#duck3a" ).animate({
-   		 	left: "+=600",
+   		 	left: "+=800",
   		}, 1000, function() {
   		});
 
   	// GO BACK HOME DUCKS
   		$( "#duck3a" ).animate({
-   			left: "-=1600",
+   			left: "-=1900",
   			}, 1000, function() {
-          $("#duck3a").css("display", "initial");
+          $("#duck3a").css("display", "inline-block");
  		});
 	});
 
@@ -260,53 +281,54 @@ $(document).ready(function() {
   		});
 
   		$( "#duck3b" ).animate({
-   		 	left: "+=400",
+   		 	left: "+=500",
   		}, 2000, function() {
   		});
 
   		$( "#duck3b" ).animate({
-   		 	left: "+=400",
-  		}, 1000, function() {
-  		});
-
-  		$( "#duck3b" ).animate({
-   		 	left: "+=700",
-  		}, 1500, function() {
-  		});
-
-  	// GO BACK HOME DUCKS
-  		$( "#duck3b" ).animate({
-   			left: "-=1600",
-  			}, 1000, function() {
- 		});
-	});
-
-	$( "#clickme" ).click(function() {
-  		$( "#duck3c" ).animate({
-   		 	left: "+=700",
-  		}, 1000, function() {
-  		});
-
-  		$( "#duck3c" ).animate({
-   		 	left: "+=100",
-  		}, 2000, function() {
-  		});
-
-  		$( "#duck3c" ).animate({
-  			left: "+=200",
-  		}, 2000, function() {
-  		});
-
-  		$( "#duck3c" ).animate({
    		 	left: "+=600",
   		}, 1000, function() {
   		});
 
+  		$( "#duck3b" ).animate({
+   		 	left: "+=700",
+  		}, 1500, function() {
+  		});
+
+  	// GO BACK HOME DUCKS
+  		$( "#duck3b" ).animate({
+   			left: "-=1900",
+  			}, 1000, function() {
+  	      $("#duck3b").css("display", "inline-block");
+ 		});
+	});
+
+	$( "#clickme" ).click(function() {
+  		$( "#duck3c" ).animate({
+   		 	left: "+=700",
+  		}, 1000, function() {
+  		});
+
+  		$( "#duck3c" ).animate({
+   		 	left: "+=100",
+  		}, 2000, function() {
+  		});
+
+  		$( "#duck3c" ).animate({
+  			left: "+=400",
+  		}, 2000, function() {
+  		});
+
+  		$( "#duck3c" ).animate({
+   		 	left: "+=700",
+  		}, 1000, function() {
+  		});
+
   	// GO BACK HOME DUCKS
   		$( "#duck3c" ).animate({
-   			left: "-=1600",
+   			left: "-=1900",
   			}, 1000, function() {
-          $("#duck3c").css("display", "initial");
+          $("#duck3c").css("display", "inline-block");
  		});
 	});
 
@@ -318,12 +340,12 @@ $(document).ready(function() {
   		});
 
   		$( "#duck4a" ).animate({
-   		 	left: "+=600",
+   		 	left: "+=800",
   		}, 2000, function() {
   		});
 
   		$( "#duck4a" ).animate({
-   		 	left: "+=100",
+   		 	left: "+=200",
   		}, 2000, function() {
   		});
 
@@ -334,16 +356,16 @@ $(document).ready(function() {
 
   		// GO BACK HOME DUCKS
   		$( "#duck4a" ).animate({
-   			left: "-=1600",
+   			left: "-=1900",
   			}, 1000, function() {
-          $("#duck4a").css("display", "initial");
+          $("#duck4a").css("display", "inline-block");
  		});
 	});
 
 
 	$( "#clickme" ).click(function() {
   		$( "#duck4b" ).animate({
-   		 	left: "+=300",
+   		 	left: "+=500",
   		}, 500, function() {
   		});
 
@@ -358,15 +380,15 @@ $(document).ready(function() {
   		});
 
   		$( "#duck4b" ).animate({
-   		 	left: "+=600",
+   		 	left: "+=700",
   		}, 2000, function() {
   		});
 
   	// GO BACK HOME DUCKS
   		$( "#duck4b" ).animate({
-   			left: "-=1600",
+   			left: "-=1900",
   			}, 1000, function() {
-          $("#duck4b").css("display", "initial");
+          $("#duck4b").css("display", "inline-block");
  		});
 
 	});
@@ -378,7 +400,7 @@ $(document).ready(function() {
   		});
 
   		$( "#duck4c" ).animate({
-   		 	left: "+=600",
+   		 	left: "+=800",
   		}, 2000, function() {
   		});
 
@@ -388,14 +410,15 @@ $(document).ready(function() {
   		});
 
   		$( "#duck4c" ).animate({
-   		 	left: "+=700",
+   		 	left: "+=800",
   		}, 1500, function() {
   		});
 
   	// GO BACK HOME DUCKS
   		$( "#duck4c" ).animate({
-   			left: "-=1600",
+   			left: "-=1900",
   			}, 1000, function() {
+  		  $("#duck4c").css("display", "inline-block");
  		});
 
 	});
